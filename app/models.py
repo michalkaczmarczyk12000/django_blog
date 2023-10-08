aed08b6d2ce878dd3a101f03351c06e64ce39e42
michal <m.kaczmarczyk12000@gmail.com>
2023-10-08 18:43:44 +0200

    created database model of image

--- app/models.py
+++ app/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return "user_{0}/{1}".format(instance.user.id, filename)
+
+
+class Image(models.Model):
+    uploaded_image = models.ImageField()
+    owner_id = models.IntegerField()
